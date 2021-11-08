e41717457a0702ee5ab63fa0f1245eb6259ed8e7
Etienne Wortham <etiennewortham@gmail.com>
2021-11-08 12:46:16 +0900

    not so good css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  def ask
+  end
+
+  def answer
+    @answer = ''
+    if params[:question] == 'I am going to work'
+      @answer = 'Great!'
+    elsif params[:question].include? '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
 end
+
+# Rails.application.routes.draw do
+#   # verb 'url', to: 'controller#action'
+#   root to: 'pages#home'
+#   get 'about', to: 'pages#about'
+#   get 'contact', to: 'pages#contact'
+# end
